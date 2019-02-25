dd4cb383f7eaa52fa82f6b87919446ea081a2b8c
Garen Crownguard <22408776+Garen7@users.noreply.github.com>
2019-02-25 12:53:53 -0600

    WIP enemies

---
+++ CampaignTrip/Assets/Multiplayer/Battle/BattleController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BattleController : NetworkBehaviour
+{
+	public static BattleController instance;
+	[Tooltip("Groups of enemies to spawn together.")]
+	public GameObject[][] waves;
+	protected int waveIndex = 0;
+	public List<Enemy> enemies;
+
+	protected void Start()
+	{
+		if (instance)
+			throw new System.Exception("There can only be one BattleController.");
+		instance = this;
+	}
+
+	protected void Win()
+	{
+		//TODO
+	}
+
+	[Command]
+	public void CmdTryEndWave()
+	{
+		foreach (Enemy e in enemies)
+			if (e.isAlive)
+				return;
+		CmdSpawnNewWave();
+	}
+
+	[Command]
+	public void CmdSpawnNewWave()
+	{
+		//clear old wave(this will be destroyed on the clients too automatically)
+		foreach (Enemy e in enemies)
+			Destroy(e.gameObject);
+		enemies.Clear();
+
+		//Are all the waves done with?
+		if(waveIndex == waves.Length)
+		{
+			Win();
+			return;
+		}
+
+		//Spawn the next wave then
+		foreach(GameObject g in waves[waveIndex])
+		{
+			GameObject newEnemy = Instantiate(g);
+			enemies.Add(newEnemy.GetComponent<Enemy>());
+			NetworkServer.Spawn(newEnemy);
+		}
+		waveIndex++;
+	}
+}
---
+++ CampaignTrip/Assets/Multiplayer/Battle/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Enemy : NetworkBehaviour
+{
+	[SerializeField][SyncVar]
+	protected int health = 100;
+
+	public bool isAlive { get { return health > 0; } }
+
+	private void OnMouseUpAsButton()
+	{
+		AdjustHealth(-20);
+	}
+
+	public void AdjustHealth(int adjustment)
+	{
+		health += adjustment;
+		if (!isAlive)
+			Die();
+	}
+
+	protected void Die()
+	{
+		BattleController.instance.CmdTryEndWave();
+		gameObject.SetActive(false);
+	}
+}
