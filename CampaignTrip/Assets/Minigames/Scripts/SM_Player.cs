2a155d5daababe854fc7b97e136b4a5d66a7410d
jomanrones <32491852+jomanrones@users.noreply.github.com>
2019-02-26 22:43:19 -0600

    Code to add camera to player

--- CampaignTrip/Assets/Minigames/Scripts/SM_Player.cs
+++ CampaignTrip/Assets/Minigames/Scripts/SM_Player.cs
@@ -8,37 +8,49 @@ public class SM_Player : NetworkBehaviour
     public float speed = 3;
     public bool localAuthority;
 
+    [SyncVar]
+    public int playernum;
+
     Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        if (playernum == PersistentPlayer.localAuthority.playerNum)
+        {
+            localAuthority = true;
+            MinigameManager.Instance.mainCamera.transform.parent = gameObject.transform;
+            MinigameManager.Instance.mainCamera.transform.localPosition = new Vector3(0, 0, -10f);
+        }
     }
 
     void FixedUpdate()
     {
-        Vector3 velocity = new Vector3();
+        if (localAuthority)
+        {
+            Vector3 velocity = new Vector3();
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            velocity += transform.up;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            velocity += transform.right;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            velocity += -transform.right;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            velocity += -transform.up;
-        }
+            if (Input.GetKey(KeyCode.W))
+            {
+                velocity += transform.up;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                velocity += transform.right;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                velocity += -transform.right;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                velocity += -transform.up;
+            }
 
-        velocity = velocity.normalized * speed;
-        rb.velocity = velocity;
+            velocity = velocity.normalized * speed;
+            rb.velocity = velocity;
+        }
 
     }
 }
--- CampaignTrip/Assets/Scripts/MinigameManager.cs
+++ CampaignTrip/Assets/Scripts/MinigameManager.cs
@@ -8,23 +8,49 @@ public class MinigameManager : MonoBehaviour
     public GameObject playerPrefab;
     public List<Transform> spawnPoints;
 
+    public Camera mainCamera;
+    public static MinigameManager Instance;
+
     private void Start()
     {
+        Instance = this;
+
         if (NetworkWrapper.IsHost && playerPrefab != null)
         {
             SpawnPlayers();
         }
     }
 
+    private void OnDestroy()
+    {
+        Instance = null;
+    }
+
     public virtual void SpawnPlayers()
     {
-        for (int i = 0; i < PersistentPlayer.players.Count; i++)
-        {
-            GameObject obj = Instantiate(playerPrefab);
-            obj.transform.position = spawnPoints[i].position;
+        //var i = 0;
+        //foreach(PersistentPlayer player in PersistentPlayer.players)
+        //{
+        //    GameObject obj = Instantiate(playerPrefab);
+        //    obj.transform.position = spawnPoints[i].position;
 
-            //NetworkSpawner.Instance.NetworkSpawn(obj);
-            NetworkSpawner.Instance.NetworkSpawn(obj, PersistentPlayer.players[i].gameObject);
-        }
+        //    obj.GetComponent<SM_Player>().playernum = player.playerNum;
+        //    NetworkSpawner.Instance.NetworkSpawn(obj);
+        //    i++;
+        //}
+
+        var player = PersistentPlayer.players[0];
+        GameObject obj = Instantiate(playerPrefab);
+        obj.transform.position = spawnPoints[0].position;
+
+        obj.GetComponent<SM_Player>().playernum = player.playerNum;
+        NetworkSpawner.Instance.NetworkSpawn(obj);
+
+        player = PersistentPlayer.players[1];
+        obj = Instantiate(playerPrefab);
+        obj.transform.position = spawnPoints[1].position;
+
+        obj.GetComponent<SM_Player>().playernum = player.playerNum;
+        NetworkSpawner.Instance.NetworkSpawn(obj);
     }
 }
