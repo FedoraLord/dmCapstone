360a3aeed5d9c2488e291ccddadd46b88dcc124e
jomanrones <32491852+jomanrones@users.noreply.github.com>
2019-03-22 22:13:13 -0500

    Build settings changed

--- CampaignTrip/Assets/Minigames/Scripts/CS_Card.cs
+++ CampaignTrip/Assets/Minigames/Scripts/CS_Card.cs
@@ -1,19 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
 public class CS_Card : NetworkBehaviour
 {
+    public Sprite sprite;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!sprite)
+        {
+            GetComponent<SpriteRenderer>().sprite = sprite;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnMouseDown()
+    {
+        Console.WriteLine("Clicked on Card");
     }
 }
